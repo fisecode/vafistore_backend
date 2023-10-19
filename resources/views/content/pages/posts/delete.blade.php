31ff4be91543721b1d7c112e01931de243eb50f3
fisecode <akhmatfikriseptiawan@gmail.com>
2023-10-19 18:05:44 +0700

    update

--- resources/views/content/pages/posts/delete.blade.php
+++ resources/views/content/pages/posts/delete.blade.php
@@ -1,7 +1,11 @@
-<form action="{{ route('user.destroy', $post->id) }}" method="post">
-  @csrf
-  @method('DELETE')
-  <h5 class="text-center">Are you sure you want to delete ?</h5>
-  <button type="button" class="btn btn-warning" data-dismiss="modal">Cancel</button>
-  <button type="submit" class="btn btn-light">Yes, Delete User</button>
+<form action="{{ route('post.destroy', $post->id) }}" method="post">
+  <div class="modal-body">
+    @csrf
+    @method('DELETE')
+    <h5 class="text-center">Are you sure you want to delete?</h5>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-warning" data-dismiss="modal">Cancel</button>
+    <button type="submit" class="btn btn-light">Yes, Delete User</button>
+  </div>
 </form>
